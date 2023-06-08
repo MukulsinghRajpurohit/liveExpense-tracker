d97ab8b9f1118f44f4c2c67aade7325f83e39077
Mukulsingh <mukulsinghrajpurohit@gmail.com>
2023-06-08 19:57:13 +0530

    Updates

---
+++ precache-manifest.bdee881e43fed4635d520fbadd7d94e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5501e2155ff01dd79be3402f4c82d25c",
+    "url": "/liveExpense-tracker/index.html"
+  },
+  {
+    "revision": "44c4bd0c5d21078f0965",
+    "url": "/liveExpense-tracker/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "44c4bd0c5d21078f0965",
+    "url": "/liveExpense-tracker/static/js/2.98f8c179.chunk.js"
+  },
+  {
+    "revision": "df2659f20723d9e518ad965c595b69ee",
+    "url": "/liveExpense-tracker/static/js/2.98f8c179.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c47da6f482f0f8946438",
+    "url": "/liveExpense-tracker/static/js/main.6f456083.chunk.js"
+  },
+  {
+    "revision": "4e03f4410342e7d416f4",
+    "url": "/liveExpense-tracker/static/js/runtime-main.d410610b.js"
+  }
+]);
